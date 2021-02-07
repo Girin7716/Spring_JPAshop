17212b8056319208144913ff1825f2e4d6eef003
Girin7716 <k3832580@naver.com>
2021-02-07 16:33:57 +0900

    order service development

--- jpashop/src/main/java/jpabook/jpashop/domain/Orderitem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Orderitem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -10,6 +12,7 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Orderitem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -25,6 +28,9 @@ public class Orderitem {
 
     private int orderPrice; //주문 가격
     private int count;  //주문 수량
+
+//    protected Orderitem() {
+//    }
 
     //==생성 메서드==//
     public static Orderitem createOrderItem(Item item, int orderPrice, int count){
@@ -42,7 +48,8 @@ public class Orderitem {
         getItem().addStock(count);
     }
 
-    //==조회 로직==//
+
+//==조회 로직==//
 
     /**
      * 주문상품 전체 가격 조회
